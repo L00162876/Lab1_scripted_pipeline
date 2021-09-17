3bdd0b332437ff80334f72772d0340a38ade46db
L00162876 <L00162876@student.lyit.ie>
2021-09-17 06:30:36 +0100

    Initial commit

---
+++ src/main/java/calculator_lyit/App.java
@@ -0,0 +1,13 @@
+package calculator_lyit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
